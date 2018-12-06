28d7869d27479b301fabac57cb53e1177740e931
ortizjs <jonnatano94@gmail.com>
2018-12-05 16:14:49 -0800

    Setup fronted files

--- skeleton/app/assets/javascripts/bundle.js
+++ skeleton/app/assets/javascripts/bundle.js
@@ -102,7 +102,7 @@ __webpack_require__.r(__webpack_exports__);
 
 
 document.addEventListener('DOMContentLoaded', function () {
-  var rooEl = document.getElementById('root');
+  var rootEl = document.getElementById('root');
   react_dom__WEBPACK_IMPORTED_MODULE_1___default.a.render(react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement("h1", null, "Pokedex"), rootEl);
 });
 
--- skeleton/frontend/pokedex.jsx
+++ skeleton/frontend/pokedex.jsx
@@ -2,6 +2,6 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 
 document.addEventListener('DOMContentLoaded', () => {
-  const rooEl = document.getElementById('root');
+  const rootEl = document.getElementById('root');
   ReactDOM.render(<h1>Pokedex</h1>, rootEl);
 });
