2b49b0b7dc858a5b2c84fa0d2224c261f32edb5a
ortizjs <jonnatano94@gmail.com>
2018-12-06 13:52:00 -0800

    Add link path to index items

--- skeleton/app/assets/javascripts/bundle.js
+++ skeleton/app/assets/javascripts/bundle.js
@@ -237,6 +237,7 @@ var mapDispatchToProps = function mapDispatchToProps(dispatch) {
 __webpack_require__.r(__webpack_exports__);
 /* harmony import */ var react__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! react */ "./node_modules/react/index.js");
 /* harmony import */ var react__WEBPACK_IMPORTED_MODULE_0___default = /*#__PURE__*/__webpack_require__.n(react__WEBPACK_IMPORTED_MODULE_0__);
+/* harmony import */ var react_router_dom__WEBPACK_IMPORTED_MODULE_1__ = __webpack_require__(/*! react-router-dom */ "./node_modules/react-router-dom/es/index.js");
 function _typeof(obj) { if (typeof Symbol === "function" && typeof Symbol.iterator === "symbol") { _typeof = function _typeof(obj) { return typeof obj; }; } else { _typeof = function _typeof(obj) { return obj && typeof Symbol === "function" && obj.constructor === Symbol && obj !== Symbol.prototype ? "symbol" : typeof obj; }; } return _typeof(obj); }
 
 function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } }
@@ -257,6 +258,7 @@ function _setPrototypeOf(o, p) { _setPrototypeOf = Object.setPrototypeOf || func
 
 
 
+
 var PokemonIndexItem =
 /*#__PURE__*/
 function (_React$Component) {
@@ -271,9 +273,11 @@ function (_React$Component) {
   _createClass(PokemonIndexItem, [{
     key: "render",
     value: function render() {
-      return react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement("li", null, this.props.pokemon.name, " ", react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement("img", {
+      return react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement("li", null, react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement(react_router_dom__WEBPACK_IMPORTED_MODULE_1__["Link"], {
+        to: "/pokemon/:pokemonId"
+      }, this.props.pokemon.name, react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement("img", {
         src: this.props.pokemon.image_url
-      }));
+      })));
     }
   }]);
 
--- skeleton/frontend/components/pokemon/pokemon_index_item.jsx
+++ skeleton/frontend/components/pokemon/pokemon_index_item.jsx
@@ -1,9 +1,10 @@
 import React from 'react';
+import {Link} from 'react-router-dom';
 
 class PokemonIndexItem extends React.Component {
   render () {
     return (
-      <li>{this.props.pokemon.name} <img src={this.props.pokemon.image_url} /></li>
+      <li><Link to='/pokemon/:pokemonId'>{this.props.pokemon.name}<img src={this.props.pokemon.image_url} /></Link></li>
     );
   }
 }
