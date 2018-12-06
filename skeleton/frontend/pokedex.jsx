a43aed09ad48eda098693a1e871e3ff72983d474
ortizjs <jonnatano94@gmail.com>
2018-12-05 16:11:26 -0800

    Setup entry file

---
+++ skeleton/frontend/pokedex.jsx
@@ -0,0 +1,7 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+document.addEventListener('DOMContentLoaded', () => {
+  const rooEl = document.getElementById('root');
+  ReactDOM.render(<h1>Pokedex</h1>, rootEl);
+});
